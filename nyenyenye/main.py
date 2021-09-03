9d8e44cc0fb82b0838158e47825a25f12a50b074
zsbati <zbati123@gmail.com>
2021-09-03 18:29:55 +0100

    Update main.py

--- nyenyenye/main.py
+++ nyenyenye/main.py
@@ -1,4 +1,8 @@
-i = 1
-while i <= 20:
-    print(i*i)
-    i += 1
+
+
+
+
+words = {"mother": "madre", "father": "padre",
+         "grandmother": "abuela", "grandfather": "abuelo"}
+
+print(words.items())
