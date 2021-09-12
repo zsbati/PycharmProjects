9732b2491dec6d6125e6e32736e0454f4630ae78
zsbati <zbati123@gmail.com>
2021-09-12 19:24:12 +0100

    new projects

---
+++ Markdown Editor/Markdown Editor/task/editor.py
@@ -0,0 +1,12 @@
+# write your code here
+print('''# John Lennon
+or ***John Winston Ono Lennon*** was one of *The Beatles*. Here are the songs he wrote I like the most: * Imagine
+* Norwegian Wood
+* Come Together
+* In My Life
+* ~~Hey Jude~~ (that was *McCartney*)
+## Examples
+Take a look at the snippet below: * Abbey Road
+* Rubber Soul
+* Revolver
+* A Hard Day's Night''')
--- nyenyenye/main.py
+++ nyenyenye/main.py
@@ -1,4 +1,13 @@
-print("I am = I'm")
+# import string library function
+import string
+
+# Storing the value in variable result
+result = string.digits
+
+# Printing the value
+print(help(string.digits))
+'''print("I am = I'm")
 print("I have = I've")
 print("I have = I've")
-print("I had / would = I'd")
+print("I had / would = I'd")'''
+
